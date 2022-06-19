7bb2ef18d3f1918606180b368e6609c3912787f8
yellow_may <yellow.may.21@gmail.com>
2022-06-19 21:14:45 +0100

    Init

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import { Footer } from 'components';
+
+const MyApp = () => {
+	return (
+		<Fragment>
+			<main className='chart-app'></main>
+
+			<Footer />
+		</Fragment>
+	);
+};
+
+export default MyApp;
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,19 @@
+import Styles from './styles.module.css';
+
+const Footer = () => {
+	return (
+		<footer className={Styles.attribution}>
+			Challenge by&nbsp;
+			<a href='https://www.frontendmentor.io?ref=challenge' target='_blank' rel='noreferrer'>
+				Frontend Mentor
+			</a>
+			. Coded by&nbsp;
+			<a href='https://yellow-may.vercel.app' target='_blank' rel='noreferrer'>
+				Yellow May
+			</a>
+			.
+		</footer>
+	);
+};
+
+export default Footer;
